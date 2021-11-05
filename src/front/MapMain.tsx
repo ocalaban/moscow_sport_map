f195326a005dc7c78b8c97e3efab797689addf00
Sey-Uki <abakarova.diana99@gmail.com>
2021-11-05 12:27:35 +0300

    corrections

--- src/front/MapMain.tsx
+++ src/front/MapMain.tsx
@@ -1,24 +1,26 @@
-import * as React from "react";
-import EventEmitter from "events";
+import * as React from 'react';
+import EventEmitter from 'events';
 
-const DG = require("2gis-maps");
-const XLSX = require("xlsx");
+const DG = require('2gis-maps');
+const XLSX = require('xlsx');
 
-import { getInterjacentColorStr, IDistrict, IObj, IRGBA } from "./types";
+import { getInterjacentColorStr, IDistrict, IObj, IRGBA } from './types';
 
-import { spr_affiinity, spr_sport, spr_zonetype } from "./mock/sprs";
-import { AnyArray } from "immer/dist/internal";
+import { spr_affiinity, spr_sport, spr_zonetype } from './mock/sprs';
+import { AnyArray } from 'immer/dist/internal';
 
 interface IMapMainProps {
-  objs: IObj[];
-  emitter: EventEmitter;
-  isPopulationLayer?: boolean;
-  districts: IDistrict[];
-}
+  objs: IObj[], 
+  
+  
+  emitter: EventEmitter,
+  isPopulationLayer?: boolean,
+  districts: IDistrict[],
+};
 
 interface IMapMainState {
-  pointsMatrix: Array<AnyArray>;
-}
+  pointsMatrix: Array<AnyArray>,
+};
 
 const clusterParams = {
   spiderfyOnMaxZoom: false,
@@ -27,7 +29,7 @@ const clusterParams = {
   disableClusteringAtZoom: 18,
 };
 
-const GRID_SIZE = 10; 
+const GRID_SIZE = 10;
 
 export default class MapMain extends React.Component<
   IMapMainProps,
@@ -46,7 +48,7 @@ export default class MapMain extends React.Component<
       pointsMatrix: [],
     };
 
-    props.emitter.on("clearCircles", () => {
+    props.emitter.on('clearCircles', () => {
       this.circles.forEach((circle) => {
         circle.removeFrom(this.map);
       });
@@ -66,11 +68,11 @@ export default class MapMain extends React.Component<
                 </div>
                 <div class="fieldCont">
                     <div class="label">Ведомственная принадлежность</div>
-                    <div class="value">${obj.org || "-"}</div>
+                    <div class="value">${obj.org || '-'}</div>
                 </div>
                 <div class="fieldCont">
                     <div class="label">Общая площадь, кв.м.</div>
-                    <div class="value">${obj.square || "-"}</div>
+                    <div class="value">${obj.square || '-'}</div>
                 </div>
                 <div class="section">`;
 
@@ -88,21 +90,21 @@ export default class MapMain extends React.Component<
                     </div>
                     <div class="fieldCont">
                         <div class="label">Тип</div>
-                        <div class="value">${zonetypeName || "-"}</div>
+                        <div class="value">${zonetypeName || '-'}</div>
                     </div>
                     <div class="fieldCont">
                         <div class="label">Площадь, кв.м.</div>
-                        <div class="value">${part.square || "-"}</div>
+                        <div class="value">${part.square || '-'}</div>
                     </div>
                     <div class="fieldCont">
                         <div class="label">Виды спорта</div>
                         <div class="sports">
                             ${part.roles
-                              .map(
-                                (sportId) =>
-                                  `<div class="value">${spr_sport[sportId]}</div>`
-                              )
-                              .join("\n")}
+          .map(
+            (sportId) =>
+              `<div class="value">${spr_sport[sportId]}</div>`
+          )
+          .join('\n')}
                         </div>
                     </div>
                 `;
@@ -147,9 +149,9 @@ export default class MapMain extends React.Component<
     var wb = XLSX.utils.book_new();
 
     // Name your sheet
-    XLSX.utils.book_append_sheet(wb, binaryWS, "Points values");
+    XLSX.utils.book_append_sheet(wb, binaryWS, 'Points values');
     // export your excel
-    XLSX.writeFile(wb, "points.xlsx");
+    XLSX.writeFile(wb, 'points.xlsx');
   }
 
   nearestTenObj(lat: number, lng: number) {
@@ -194,7 +196,7 @@ export default class MapMain extends React.Component<
   generatePoints = () => {
     if (this.state.pointsMatrix.length > 0) {
       this.setState({
-          pointsMatrix: [],
+        pointsMatrix: [],
       });
     }
     let lng = 55.993004814153956;
@@ -215,123 +217,129 @@ export default class MapMain extends React.Component<
   }
 
   render() {
-    return (<>
-      <div className="inputContainer">
-        <button onClick={this.generatePoints}>Ближайшие 10 объектов</button>
-      </div> 
-      <div
-        id="map"
-        style={{ width: "100%", height: "100%" }}
-        ref={(node) => {
-          if (node) {
-            if (!this.map) {
-              this.map = DG.map("map", {
-                center: [55.64, 37.8],
-                zoom: 13,
-              });
-
-              this.map.on("click", (event) => {
-                let sum = 0;
-                let count = 0;
-                let wasIn = false;
-
-                this.nearestObj(event);
-
-                this.circles.forEach((circle) => {
-                  let dist = this.map.distance(event.latlng, circle._latlng);
-                  let isIn = dist <= circle.options.radius;
-
-                  if (isIn) {
-                    wasIn = true;
-                    sum += circle.square || 0; // на всякий случай, по идее 0 всегда
-                    count += 1;
-                  }
+    return (
+      <>
+        <div className='inputContainer'>
+          <button onClick={this.generatePoints}>Ближайшие 10 объектов</button>
+        </div>
+        <div
+          id='map'
+          style={{ width: '100%', height: '100%' }}
+          ref={(node) => {
+            if (node) {
+              if (!this.map) {
+                this.map = DG.map('map', {
+                  center: [55.64, 37.8],
+                  zoom: 13,
                 });
 
-                if (wasIn) {
-                  let popup = DG.popup()
-                    .setLatLng(event.latlng)
-                    .setContent(
-                      `<div><div>Объектов в доступе: ${count}</div><div>Общая площадь, кв.м.: ${sum}</div></div>`
-                    )
-                    .openOn(this.map);
-                }
-              });
-            }
+                this.map.on('click', (event) => {
+                  let sum = 0;
+                  let count = 0;
+                  let wasIn = false;
 
-            if (this.cluster) {
-              this.map.removeLayer(this.cluster);
-            }
+                  this.nearestObj(event);
 
-            let cluster = DG.markerClusterGroup(clusterParams);
+                  this.circles.forEach((circle) => {
+                    let dist = this.map.distance(event.latlng, circle._latlng);
+                    let isIn = dist <= circle.options.radius;
 
-            this.props.objs.forEach((obj) => {
-              let popupContent = this.formPopupInnerHTML(obj);
+                    if (isIn) {
+                      wasIn = true;
+                      sum += circle.square || 0; // на всякий случай, по идее 0 всегда
+                      count += 1;
+                    }
+                  });
 
-              let marker = DG.marker(obj);
-              marker.bindPopup(popupContent);
-
-              cluster.addLayer(marker);
-              let that = this;
-
-              let radii = [5000, 3000, 1000, 500];
-              let radius = radii[obj.affinityId - 1];
-
-              let step;
-
-              if (obj.square && Math.log10(obj.square) > 1) {
-                step = Math.floor(Math.log(obj.square));
-              } else {
-                step = 1;
+                  if (wasIn) {
+                    let popup = DG.popup()
+                      .setLatLng(event.latlng)
+                      .setContent(
+                        `<div><div>Объектов в доступе: ${count}</div><div>Общая площадь, кв.м.: ${sum}</div></div>`
+                      )
+                      .openOn(this.map);
+                  }
+                });
               }
 
-              const stepCount = 14;
+              if (this.cluster) {
+                this.map.removeLayer(this.cluster);
+              }
 
-              const rgb1 = [255, 0, 0, 1] as IRGBA;
-              const rgb2 = [0, 255, 0, 1] as IRGBA;
+              let cluster = DG.markerClusterGroup(clusterParams);
 
-              let rgbStr = getInterjacentColorStr(step, stepCount, rgb1, rgb2);
-              marker.on("click", function () {
-                let circle = DG.circle([obj.lat, obj.lng], {
-                  radius,
-                  color: rgbStr,
-                }).addTo(that.map);
-                circle.square = obj.square;
+              this.props.objs.forEach((obj) => {
+                let popupContent = this.formPopupInnerHTML(obj);
 
-                that.circles.push(circle);
-              });
-            });
+                let marker = DG.marker(obj);
+                marker.bindPopup(popupContent);
 
-            this.map.addLayer(cluster);
-            this.cluster = cluster;
+                cluster.addLayer(marker);
+                let that = this;
 
-            this.polys.forEach((poly) => {
-              poly.removeFrom(this.map);
-            });
+                let radii = [5000, 3000, 1000, 500];
+                let radius = radii[obj.affinityId - 1];
 
-            if (this.props.isPopulationLayer) {
-              this.props.districts.forEach((district) => {
-                const limit = 20;
+                let step;
 
-                let dense = Math.floor(district[3] / district[2] / 10);
-                if (dense > limit) {
-                  dense = limit;
+                if (obj.square && Math.log10(obj.square) > 1) {
+                  step = Math.floor(Math.log(obj.square));
+                } else {
+                  step = 1;
                 }
 
-                const rgb1 = [0, 0, 255, 1] as IRGBA;
-                const rgb2 = [255, 0, 0, 1] as IRGBA;
+                const stepCount = 14;
 
-                let rgbStr = getInterjacentColorStr(dense, limit, rgb1, rgb2);
+                const rgb1 = [255, 0, 0, 1] as IRGBA;
+                const rgb2 = [0, 255, 0, 1] as IRGBA;
 
-                let poly = DG.polygon(district[5][0], { color: rgbStr }).addTo(
-                  this.map
+                let rgbStr = getInterjacentColorStr(
+                  step,
+                  stepCount,
+                  rgb1,
+                  rgb2
                 );
-                this.polys.push(poly);
+                marker.on('click', function () {
+                  let circle = DG.circle([obj.lat, obj.lng], {
+                    radius,
+                    color: rgbStr,
+                  }).addTo(that.map);
+                  circle.square = obj.square;
+
+                  that.circles.push(circle);
+                });
               });
+
+              this.map.addLayer(cluster);
+              this.cluster = cluster;
+
+              this.polys.forEach((poly) => {
+                poly.removeFrom(this.map);
+              });
+
+              if (this.props.isPopulationLayer) {
+                this.props.districts.forEach((district) => {
+                  const limit = 20;
+
+                  let dense = Math.floor(district[3] / district[2] / 10);
+                  if (dense > limit) {
+                    dense = limit;
+                  }
+
+                  const rgb1 = [0, 0, 255, 1] as IRGBA;
+                  const rgb2 = [255, 0, 0, 1] as IRGBA;
+
+                  let rgbStr = getInterjacentColorStr(dense, limit, rgb1, rgb2);
+
+                  let poly = DG.polygon(district[5][0], {
+                    color: rgbStr,
+                  }).addTo(this.map);
+                  this.polys.push(poly);
+                });
+              }
             }
-          }
-        }}
-      ></div>
+          }}
+        ></div>
       </>
     );
   }
